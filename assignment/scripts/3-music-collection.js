58ae77e1ae5d2049b4501c4a24c6f21e554c05e2
Justin Kahn <71735000+justinthegithub@users.noreply.github.com>
2024-04-26 23:01:34 +0000

    6 albums added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,7 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 //Create an empty array named myCollection.
-let myCollection =[];
+let myCollection =[]
 //Create a function named addToCollection.  
 //This function should:
 //Take in a collection parameter. (This allows the function to be reused to add an album to any array of album objects.)
@@ -12,26 +12,44 @@ let myCollection =[];
 
 function addToCollection(collection, title, artist, yearPublished) {
   // your code here...
-
+let album = {
   
+  title: title, 
+  artist: artist,
+  yearPublished: yearPublished
 }
-
-
-
-
+  collection.push(album)
+  return album;
+}
 /*Add the new object to the end of the collection array.
 return the newly created object.
 Use and Test the addToCollection function:
+*/
+console.log(addToCollection(myCollection, 'The National', 'The National', 1994))
 
-Add 6 albums to the myCollection array. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
-console.log each album as added using the function's returned value.
-After all are added, console.log the myCollection array.
-Create a function named showCollection. This function should:
+console.log(addToCollection(myCollection, 'Ben Folds', 'The Best Imitation of Myself', 2011))
+
+console.log(addToCollection(myCollection, 'Modest Mouse', 'Good News for People Who Love Bad News', 2004))
+
+console.log(addToCollection(myCollection, 'Wilco', 'Yankee Foxtrot Hotel', 1996))
+
+console.log(addToCollection(myCollection, 'U2', 'Achtung Baby', 1990))
+
+console.log(addToCollection(myCollection, 'The Smile', 'Wall of Eyes', 2024)
+
+console.log(myCollection);
+
+/*Add 6 albums to the myCollection array. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+console.log each album as added using the function's returned value. */
+// After all are added, console.log the myCollection array.
+
+/*Create a function named showCollection. This function should:
 
 Take in a collection parameter. (This allows it to be reused to show any array of album objects.)
 Loop through the collection and console.log each album's information formatted within a single string, like: TITLE by ARTIST, published in YEARPUBLISHED.
-Test the showCollection function.
+Test the showCollection function.*/
 
+/*
 Add a function named findByArtist. This function should:
 
 Take in a collection parameter. Remember, we want to be able to search any collection!
@@ -40,6 +58,7 @@ Create an empty array to hold any matching results, if any.
 Loop through the collection and add any album objects with a matching artist to the array.
 Return the array with the matching results. (If no results are found, an empty array should be returned.)
 Test the findByArtist function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are returned.
+*/
 
 When testing your functions, write all tests in the JavaScript file!
 
